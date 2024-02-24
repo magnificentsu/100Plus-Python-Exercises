e41781928f902cbb1fa26b722b4386cfe948a41e
magnificentsu <sulei@magnificentsu.com>
2024-02-24 05:44:08 -0600

    24 Feb 2024

---
+++ Question 78.py
@@ -0,0 +1,15 @@
+# Please write a program to shuffle and print the list [3,6,7,8].
+
+#MY SOLUTION:
+import random
+
+my_list = [3,6,7,8]
+random.shuffle(my_list)
+print(my_list)
+
+#COURSE SOLUTION:
+import random
+
+lst = [3,6,7,8]
+random.shuffle(lst)
+print(lst)
